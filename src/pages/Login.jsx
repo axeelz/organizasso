c7b9fad1bd26ee0fcd578f1ea2e791a24b2a556d
axeelz <axelzareb@hotmail.fr>
2024-03-03 20:52:12 +0100

    styles login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import styles from "./Login.module.css";
 
 const Login = () => {
   const [username, setUsername] = useState("");
@@ -11,27 +12,35 @@ const Login = () => {
   };
 
   return (
-    <main>
-      <h1>Connexion</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Nom d'utilisateur"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Mot de passe"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Connexion</button>
-      </form>
-      <p>
-        Tu n'as pas de compte ? <Link to="/signup">Inscris toi !</Link>
-      </p>
-    </main>
+    <div className={styles.login}>
+      <div className={styles.leftContainer}></div>
+      <div className={styles.rightContainer}>
+        <div className={styles.header}>
+          <h1>Connecte toi</h1>
+          <p>pour accéder à ton compte</p>
+        </div>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <input
+            type="text"
+            placeholder="Nom d'utilisateur"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Mot de passe"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" className={styles.button}>
+            Connexion
+          </button>
+        </form>
+        <p className={styles.signup}>
+          Tu n'as pas de compte ? <Link to="/signup">Inscris toi !</Link>
+        </p>
+      </div>
+    </div>
   );
 };
 
