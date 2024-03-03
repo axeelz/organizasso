5c0ac2e260ee0fecce1f780c69f19084fb3d02bf
axeelz <axelzareb@hotmail.fr>
2024-03-03 20:09:39 +0100

    relatime time

--- src/components/MessagesList.jsx
+++ src/components/MessagesList.jsx
@@ -1,32 +1,36 @@
 import styles from "./MessagesList.module.css";
+import { formatDistance } from "date-fns";
+import { fr } from "date-fns/locale";
 
 const MessagesList = ({
   messages = [
     {
       id: 1,
       username: "johndoe",
-      date: "maintenant",
+      date: "2024-03-02T12:00:00",
       content: "This is the first message",
     },
     {
       id: 2,
       username: "janedoe",
-      date: "il y a 5 minutes",
+      date: "2024-03-03T20:12:00",
       content: "This is the second message",
     },
   ],
-}) => (
-  <section className={styles.messagesList}>
-    {messages.map((message) => (
-      <div key={message.id} className={styles.message}>
-        <div className={styles.messageHeader}>
-          <h4>@{message.username}</h4>
-          <span>{message.date}</span>
+}) => {
+  return (
+    <section className={styles.messagesList}>
+      {messages.map((message) => (
+        <div key={message.id} className={styles.message}>
+          <div className={styles.messageHeader}>
+            <h4>@{message.username}</h4>
+            <span>{formatDistance(new Date(message.date), new Date(), { addSuffix: true, locale: fr })}</span>
+          </div>
+          <p>{message.content}</p>
         </div>
-        <p>{message.content}</p>
-      </div>
-    ))}
-  </section>
-);
+      ))}
+    </section>
+  );
+};
 
 export default MessagesList;
