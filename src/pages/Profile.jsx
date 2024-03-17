8b4adaa1a45498a44ca3a2d07ae2619a25856c44
axeelz <axelzareb@hotmail.fr>
2024-03-17 22:20:01 +0100

    back button profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import MessagesList from "../components/MessagesList";
+import { IoArrowBack } from "react-icons/io5";
+import styles from "./Profile.module.css";
 
 const Profile = () => {
   const { username } = useParams();
+  const navigate = useNavigate();
 
   return (
     <main>
+      <button onClick={() => navigate(-1)} className={styles.back}>
+        <IoArrowBack />
+        <span>Retour</span>
+      </button>
       {(username && <h1>Profil de {username}</h1>) || <h1>Mon profil</h1>}
       {(username && <MessagesList username={username} />) || <MessagesList />}
     </main>
